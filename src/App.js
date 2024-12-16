56a67c4d79a389e405afdda19fb966f33e0467df
Yevhenii Danysko <emineshaban@icloud.com>
2024-12-16 19:56:11 +0200

    create card component

---
+++ src/App.js
@@ -0,0 +1,43 @@
+// import './reset.css';
+import './App.css';
+
+import React, { useState } from "react";
+import Button from './components/Card';
+
+function App() {
+  const [activeButton, setActiveButton] = useState("all"); // 'all' or 'available'
+
+  return (
+    <div className="App">
+
+      <div className="body">
+        <img src='/images/bg-cafe.jpg' className="background" alt='background' />
+        <div className='container'>
+          <img src='/images/vector.svg' className="vector" alt='vector' />
+          <div className='title-div'>
+
+            <h1>Our Collection</h1>
+            <p className='title'>Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
+          </div>
+          <div className="buttons">
+            <button
+              className={activeButton === "all" ? "isActiv" : ""}
+              onClick={() => setActiveButton("all")}
+            >
+              All Products
+            </button>
+            <button
+              className={activeButton === "available" ? "isActiv" : ""}
+              onClick={() => setActiveButton("available")}
+            >
+              Available Now
+            </button>
+            <Button></Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
